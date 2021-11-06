75f906628478be8898575465fa0fc94258d22f01
Corey W <wencorey0@gmail.com>
2021-11-06 02:36:36 -0400

    argh

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,8 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
-import 'package:wheel_of_workouts_flutter/main.dart';
+import 'package:wheel_of_workouts/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
